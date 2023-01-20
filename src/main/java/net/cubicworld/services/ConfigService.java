41f09e7ce02f58446bf7a0c84f031e186225ad16
baka <dyatlovleonid@gmail.com>
2023-01-20 17:50:39 +0300

    a

--- src/main/java/net/cubicworld/services/ConfigService.java
+++ src/main/java/net/cubicworld/services/ConfigService.java
@@ -7,15 +7,6 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public final class ConfigService {
-    public static @NotNull Properties getTestConfig() throws IOException {
-        try(InputStream stream = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("config.properties")) {
-            Properties properties = new Properties();
-            properties.load(stream);
-            return properties;
-        }
-    }
-
     public static @NotNull Properties getConfig() throws IOException {
         try(InputStream stream = Thread.currentThread().getContextClassLoader()
                 .getResourceAsStream("config.properties")) {
--- src/test/java/net/cubicworld/services/ConfigServiceTest.java
+++ src/test/java/net/cubicworld/services/ConfigServiceTest.java
@@ -6,13 +6,6 @@ import java.io.IOException;
 import java.util.Properties;
 
 class ConfigServiceTest {
-
-    @Test
-    void getTestConfig() throws IOException {
-        Properties properties = ConfigService.getTestConfig();
-        assert !properties.isEmpty();
-    }
-
     @Test
     void getConfig() throws IOException {
         Properties properties = ConfigService.getConfig();
