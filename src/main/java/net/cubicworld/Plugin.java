c73fd97ad46947154e90f323c03bc52d29cd51aa
baka <dyatlovleonid@gmail.com>
2023-01-22 16:06:07 +0300

    removed db

--- src/main/java/net/cubicworld/Plugin.java
+++ src/main/java/net/cubicworld/Plugin.java
@@ -4,19 +4,14 @@ import lombok.Getter;
 import net.cubicworld.commands.ArenaCommand;
 import net.cubicworld.events.BlockListener;
 import net.cubicworld.events.PlayerListener;
-import net.cubicworld.database.Database;
 import net.cubicworld.game.ArenaManager;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.PluginClassLoader;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.BitSet;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -25,8 +20,6 @@ import java.util.logging.Level;
 @Getter
 public class Plugin extends JavaPlugin implements Listener {
     private Properties pluginSettings;
-
-    private Database database;
 
     private ArenaManager arenaManager;
 
@@ -40,17 +33,6 @@ public class Plugin extends JavaPlugin implements Listener {
         } catch (IOException e) {
             getLogger().log(Level.SEVERE, "Unable to get configuration file");
             Bukkit.shutdown();
-            return;
-        }
-
-        database = new Database(pluginSettings);
-        try(Connection connection = database.getConnection()) {
-            if (!connection.isValid(1)) {
-                getLogger().log(Level.SEVERE, "Database connection is invalid");
-                Bukkit.shutdown();
-            }
-        } catch (SQLException e) {
-            getLogger().log(Level.SEVERE, "Failed to connect to database", e);
         }
     }
 
